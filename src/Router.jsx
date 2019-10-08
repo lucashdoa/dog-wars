97204224fae42dd603cc2ee8733bd2cc2619b33d
Lucas5990 <lucasandrade5990@gmail.com>
2019-10-07 22:23:16 -0300

    rota main

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter,Route} from 'react-router-dom';
+import {BrowserRouter,Route,Switch} from 'react-router-dom';
 import Home from './Home.jsx';
 import Main from './main/Main.jsx';
 
@@ -11,6 +11,11 @@ export default props => (
         <div>
         <Route path='/' component={Home} exact/>
         <Route path='/main' component={Main}/>
+        <Switch>
+            <Route path="/main">
+                <Main />
+            </Route>
+        </Switch>
         </div>
     </BrowserRouter>
 ); 
